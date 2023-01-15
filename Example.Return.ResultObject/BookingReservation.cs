fb02f107ab4718b5a48c8f9a75607b9394854a90
Alexandru Aldea <aldea.alex19@gmail.com>
2023-01-15 11:54:08 +0200

    Add project files.

---
+++ Example.Return.ResultObject/BookingReservation.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+
+namespace Example.Return.ResultObject
+{
+    public class BookingReservation
+    {
+        private int[] bookings = new int[] { 1, 2, 3, 4, 5, 6 };    
+        public Result<string> FindBookingById(int? bookingId)
+        {
+            if(bookingId is null or 0)
+            {
+                return Result.Fail<string>($"The booking is null or 0");
+            }
+
+            int booking = bookings.FirstOrDefault(b => b.Equals(bookingId));    
+
+            return Result.Ok($"The booking with id: {booking} was found.");
+        }
+    }
+}
---
+++ Example.Return.ResultObject/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Example.Return.ResultObject;
+
+BookingReservation bookingReservation = new BookingReservation();
+var result = bookingReservation.FindBookingById(7);
+
+if (result.IsSuccess)
+{
+    Console.WriteLine(result.Value);
+}
+else
+{
+    Console.WriteLine(result.Reasons[0].Message);
+}
+
+
+Console.ReadKey();
